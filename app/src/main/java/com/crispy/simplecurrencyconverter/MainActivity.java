92c44155960ec4ecff0312c6062ad20ffa65e74c
Icaros <miska@ronin.fi>
2016-04-21 11:32:27 +0300

    upup

---
+++ app/src/main/java/com/crispy/simplecurrencyconverter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.crispy.simplecurrencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void convertClicked(View view) {
+        EditText dollarField = (EditText) findViewById(R.id.moneyTextField);
+        Double dollarAmount = Double.parseDouble(dollarField.getText().toString());
+        Double pound = 0.695889;
+
+        Toast.makeText(getApplicationContext(), "£" + (dollarAmount * pound), Toast.LENGTH_LONG).show();
+    }
+}
